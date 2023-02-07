8b8e752c2d9c42d3fb30704bca90341b5947f1f1
LoopFruits <Kensworldc@gmail.com>
2023-02-07 12:46:11 -0500

    added nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav(){
+    return(
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Sportlytics
+            </h1>
+        </div>
+    )
+}
