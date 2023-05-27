aaf3a0344295fa1c409bec6884358a735386ad48
LoopFruits <Kensworldc@gmail.com>
2023-05-27 10:17:52 -0400

    fixed grammer mistakes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 
-const Navbar = () => {
+const Nav = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -16,4 +15,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar;
+export default Nav;
